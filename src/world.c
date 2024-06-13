6bf03097bb09e7b87180087dabc6eee5c84893ed
nathansmith117 <nathansmith117@sdf.org>
2024-06-12 22:06:54 -0600

    Adding npcs

--- src/world.c
+++ src/world.c
@@ -6,7 +6,8 @@
 void initWorld(World* world)
 {
     SetRandomSeed(time(NULL));
-    
+
+    // Generate random world.
     for (int row = 0; row < WORLD_ROWS; ++row)
     {
         for (int col = 0; col < WORLD_COLS; ++col)
@@ -28,6 +29,23 @@ void initWorld(World* world)
                     break;
             }
         }
+    }
+
+    // Generate npcs.
+    world->npcCount = GetRandomValue(NPCS_MIN, NPCS_MAX);
+
+    for (int i = 0; i < world->npcCount; ++i)
+    {
+        Npc* npc = &world->npcs[i];
+        
+        npc->position = (Vector2){GetRandomValue(0, WORLD_MAX_X), GetRandomValue(0, WORLD_MAX_Y)};
+        npc->direction = Vector2Zero();
+        
+        Color colors[] = {RED, BLUE, GREEN, YELLOW, ORANGE, GRAY, WHITE};
+        npc->color = colors[GetRandomValue(0, sizeof(colors) / sizeof(Color) - 1)];
+
+        npc->targetRow = -1;
+        npc->targetCol = -1;
     }
 
     world->showGrid = false;
@@ -65,6 +83,39 @@ void drawWorldStructure(StructureID structureID, int row, int col, Game* game)
     }
 }
 
+void updateNpc(World* world, Npc* npc, Game* game)
+{
+    bool retarget = false;
+    Vector2 target = (Vector2){npc->targetCol * WORLD_BLOCK_SIZE, npc->targetRow * WORLD_BLOCK_SIZE};
+
+    // Target hasn't been set yet.
+    if (npc->targetRow== -1)
+    {
+        retarget = true;
+    }
+
+    if (Vector2Distance(npc->position, target) <= WORLD_BLOCK_SIZE / 2.0) // At position.
+    {
+        retarget = true;
+    }
+    else
+    {
+        float frameTime = GetTime();
+        npc->position = Vector2Add(npc->position, Vector2Scale(npc->direction, frameTime * NPC_SPEED));
+    }
+
+    // Set a new target.
+    if (retarget)
+    {
+        SetRandomSeed(clock());
+        npc->targetRow = GetRandomValue(0, WORLD_ROWS);
+        npc->targetCol = GetRandomValue(0, WORLD_COLS);
+        target = (Vector2){npc->targetCol * WORLD_BLOCK_SIZE, npc->targetRow * WORLD_BLOCK_SIZE};
+        
+        npc->direction = Vector2Normalize(Vector2Subtract(target, npc->position));
+    }
+}
+
 void updateWorld(World* world, Game* game)
 {
     for (int row = 0; row < WORLD_ROWS; ++row)
@@ -81,6 +132,20 @@ void updateWorld(World* world, Game* game)
                 DrawRectangleLinesEx((Rectangle){x, y, WORLD_BLOCK_SIZE, WORLD_BLOCK_SIZE}, 1.0 / game->camera.zoom, BLUE);   
             }
         }
+    }
+
+    // Draw and update npcs.
+    Texture npcTexture = game->assets.textures[CHARACTER_TEXTURE];
+    
+    for (int i = 0; i < world->npcCount; ++i)
+    {
+        Npc* npc = &world->npcs[i];
+
+        // Draw it.
+        DrawTextureV(npcTexture, npc->position, npc->color);
+
+        // Update it.
+        updateNpc(world, npc, game);
     }
 }
 
--- src/world.h
+++ src/world.h
@@ -9,6 +9,10 @@
 #define WORLD_MAX_X (WORLD_ROWS * WORLD_BLOCK_SIZE)
 #define WORLD_MAX_Y (WORLD_COLS * WORLD_BLOCK_SIZE)
 
+#define NPCS_MIN 15
+#define NPCS_MAX 50
+#define NPC_SPEED 0.05
+
 // type structure is at each point.
 typedef enum StructureID {
     NONE_STRUCTURE,
@@ -17,8 +21,23 @@ typedef enum StructureID {
     OFFCE_STRUCTURE
 } StructureID;
 
+typedef struct Npc {
+    Vector2 position;
+    Vector2 direction;
+
+    Color color;
+
+    // The place they are heading to.
+    int targetRow;
+    int targetCol;
+} Npc;
+
 typedef struct World {
     StructureID structures[WORLD_ROWS][WORLD_COLS];
+    
+    Npc npcs[NPCS_MAX];
+    int npcCount;
+    
     bool showGrid;
 } World;
 
